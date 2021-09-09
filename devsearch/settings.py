ab7091d1ec84ced492b0aefec6b5e7a8dc7b731f
JONES LUKORITO <JONESDEELDER@gmail.com>
2021-09-09 14:59:21 +0300

    static files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -127,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR /'static'
+]
+
+MEDIA_ROOT =os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
